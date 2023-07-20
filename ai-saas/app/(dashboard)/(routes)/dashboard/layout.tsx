7ea76d4ce9ceed07336acd4fe33ab6031c96ba17
Bonkesam <bonkesam9@gmail.com>
2023-07-20 14:26:57 +0300

    Adds the menu components to the sidebar

--- ai-saas/app/(dashboard)/(routes)/dashboard/layout.tsx
+++ ai-saas/app/(dashboard)/(routes)/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import Sidebar from "@/components/Sidebar";
 import Navbar from "@/components/ui/navbar";
 
 const DashboardLayout = ({
@@ -8,9 +9,7 @@ const DashboardLayout = ({
     return (
         <div className="h-full relative">
             <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-                <div>
-                    Hello Sidebar
-                </div>
+                <Sidebar />
             </div>
             <main className="md:pl-72">
                 <Navbar />
